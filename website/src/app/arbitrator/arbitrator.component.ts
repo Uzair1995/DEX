2917162908a678796ef245384b7a2ca02c35ef14
Uzair Aleem <uzair.aleem@mparsec.com>
2018-05-21 12:14:51 +0500

    call for contract functions added in arbitrator screen

--- website/src/app/arbitrator/arbitrator.component.ts
+++ website/src/app/arbitrator/arbitrator.component.ts
@@ -14,21 +14,22 @@ export class ArbitratorComponent implements OnInit {
   contractAbi = ABI;
   web3: any;
   coinbase: string;
+  contract: any;
 
-    public owner;
-    public sellerAddress;
-    public buyerAddress;
-    public arbitratorAddress;
-    public arbitratorFees;
-    public offeredAmount;
-    
-    public isBuyerAgreeing;
-    public isSellerAgreeing;
-    public isArbitratorAgreeingForBuyer;
-    public isArbitratorAgreeingForSeller;
-    
-    public sellerAmountDeposit;
-    public buyerSecurityDeposit;
+  public owner;
+  public sellerAddress;
+  public buyerAddress;
+  public arbitratorAddress;
+  public arbitratorFees;
+  public offeredAmount;
+
+  public isBuyerAgreeing;
+  public isSellerAgreeing;
+  public isArbitratorAgreeingForBuyer;
+  public isArbitratorAgreeingForSeller;
+
+  public sellerAmountDeposit;
+  public buyerSecurityDeposit;
 
   constructor() { }
 
@@ -37,25 +38,134 @@ export class ArbitratorComponent implements OnInit {
     this.coinbase = this.web3.eth.coinbase;
   }
 
-  LoadContract(ethContractAddress: string){
-    var contract = (this.web3.eth.contract(this.contractAbi)).at(ethContractAddress);
-    console.log(contract);
-    
-    this.owner = contract.owner.call();
-    this.arbitratorAddress = contract.arbitratorAddress.call();
-    this.sellerAddress = contract.sellerAddress.call();
-    this.buyerAddress = contract.buyerAddress.call();
-    
-    this.arbitratorFees = contract.arbitratorFees.call();
-    this.offeredAmount = contract.offeredAmount.call();
+  LoadContract(ethContractAddress: string) {
+    this.contract = (this.web3.eth.contract(this.contractAbi)).at(ethContractAddress);
+    this.web3.eth.defaultAccount = this.web3.eth.accounts[0];
+    console.log(this.contract);
 
-    this.isBuyerAgreeing = contract.isBuyerAgreeing.call();
-    this.isSellerAgreeing = contract.isSellerAgreeing.call();
-    this.isArbitratorAgreeingForBuyer = contract.isArbitratorAgreeingForBuyer.call();
-    this.isArbitratorAgreeingForSeller = contract.isArbitratorAgreeingForSeller.call();
+    this.owner = this.contract.owner.call();
+    this.arbitratorAddress = this.contract.arbitratorAddress.call();
+    this.sellerAddress = this.contract.sellerAddress.call();
+    this.buyerAddress = this.contract.buyerAddress.call();
 
-    this.sellerAmountDeposit = contract.sellerAmountDeposit.call();
-    this.buyerSecurityDeposit = contract.buyerSecurityDeposit.call();
+    this.arbitratorFees = this.contract.arbitratorFees.call();
+    this.offeredAmount = this.contract.offeredAmount.call();
+
+    this.isBuyerAgreeing = this.contract.isBuyerAgreeing.call();
+    this.isSellerAgreeing = this.contract.isSellerAgreeing.call();
+    this.isArbitratorAgreeingForBuyer = this.contract.isArbitratorAgreeingForBuyer.call();
+    this.isArbitratorAgreeingForSeller = this.contract.isArbitratorAgreeingForSeller.call();
+
+    this.sellerAmountDeposit = this.contract.sellerAmountDeposit.call();
+    this.buyerSecurityDeposit = this.contract.buyerSecurityDeposit.call();
+  }
+
+  updateOwner() {
+    if (this.contract != undefined) {
+      var isUnlocked = this.web3.personal.unlockAccount(this.web3.eth.defaultAccount, "mparsec123");
+      if(isUnlocked){
+        this.contract.updateOwner("0xbCAC3E9973918eF49ec2b08325d3add3b7586a36");
+      }
+    }
+  }
+
+  updateArbitrator() {
+    if (this.contract != undefined) {
+      var isUnlocked = this.web3.personal.unlockAccount(this.web3.eth.defaultAccount, "mparsec123");
+      if(isUnlocked){
+        this.contract.updateArbitrator("0xbCAC3E9973918eF49ec2b08325d3add3b7586a36");
+      }
+    }
+  }
+
+  updateSeller() {
+    if (this.contract != undefined) {
+      var isUnlocked = this.web3.personal.unlockAccount(this.web3.eth.defaultAccount, "mparsec123");
+      if(isUnlocked){
+        this.contract.updateSeller("0xbCAC3E9973918eF49ec2b08325d3add3b7586a36");
+      }
+    }
+  }
+
+  updateBuyer() {
+    if (this.contract != undefined) {
+      var isUnlocked = this.web3.personal.unlockAccount(this.web3.eth.defaultAccount, "mparsec123");
+      if(isUnlocked){
+        this.contract.updateBuyer("0xbCAC3E9973918eF49ec2b08325d3add3b7586a36");
+      }
+    }
+  }
+
+  updateArbitratorFees() {
+    if (this.contract != undefined) {
+      var isUnlocked = this.web3.personal.unlockAccount(this.web3.eth.defaultAccount, "mparsec123");
+      if(isUnlocked){
+        this.contract.updateArbitratorFees(this.web3.toBigNumber(0.1*1000000000000000000));//0.1 ether
+      }
+    }
+  }
+
+  updateOfferedAmount() {
+    if (this.contract != undefined) {
+      var isUnlocked = this.web3.personal.unlockAccount(this.web3.eth.defaultAccount, "mparsec123");
+      if(isUnlocked){
+        this.contract.updateOfferedAmount(this.web3.toBigNumber(1*1000000000000000000));//1 ether
+      }
+    }
+  }
+
+  signerForBuyer(){
+    if (this.contract != undefined) {
+      var isUnlocked = this.web3.personal.unlockAccount(this.web3.eth.defaultAccount, "mparsec123");
+      if(isUnlocked){
+        this.contract.signerForBuyer();
+      }
+    }
+  }
+
+  signerForSeller(){
+    if (this.contract != undefined) {
+      var isUnlocked = this.web3.personal.unlockAccount(this.web3.eth.defaultAccount, "mparsec123");
+      if(isUnlocked){
+        this.contract.signerForSeller();
+      }
+    }
+  }
+
+  signerForArbitratorForBuyer(){
+    if (this.contract != undefined) {
+      var isUnlocked = this.web3.personal.unlockAccount(this.web3.eth.defaultAccount, "mparsec123");
+      if(isUnlocked){
+        this.contract.signerForArbitratorForBuyer();
+      }
+    }
+  }
+
+  signerForArbitratorForSeller(){
+    if (this.contract != undefined) {
+      var isUnlocked = this.web3.personal.unlockAccount(this.web3.eth.defaultAccount, "mparsec123");
+      if(isUnlocked){
+        this.contract.signerForArbitratorForSeller();
+      }
+    }
+  }
+
+  depositForBuyer(){
+    if (this.contract != undefined) {
+      var isUnlocked = this.web3.personal.unlockAccount(this.web3.eth.defaultAccount, "mparsec123");
+      if(isUnlocked){
+        this.contract.depositForBuyer({value:this.web3.toWei(1,'ether')});
+      }
+    }
+  }
+
+  depositForSeller(){
+    if (this.contract != undefined) {
+      var isUnlocked = this.web3.personal.unlockAccount(this.web3.eth.defaultAccount, "mparsec123");
+      if(isUnlocked){
+        this.contract.depositForSeller({value:this.web3.toWei(1,'ether')});
+      }
+    }
   }
 
 }
