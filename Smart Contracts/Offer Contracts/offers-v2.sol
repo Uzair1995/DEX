8bea456c4c3ca6ec746691a2636059481dfeb221
asad97akay <kasadullahkhalid@gmail.com>
2018-05-27 16:10:35 +0500

    offerv2.sol contract

---
+++ Smart Contracts/Offer Contracts/offers-v2.sol
@@ -0,0 +1,44 @@
+pragma solidity ^0.4.24;
+
+contract MainEscrowContract {
+    
+    struct Offer {
+        string tradingAccount;
+        string fiatCurrency;
+        uint ethQuantity;
+        uint fiatQuantity;
+        uint arbitratorFee;
+    }
+    
+    mapping (address => Offer) offers;
+    Offer [] public offerList;
+    
+    function placeOffer(address _address,string _tradingAccount, string _fiatCurrency, uint _ethQuantity,uint _fiatQuantity,uint _arbitratorFee) external {
+        Offer storage offerer = offers[_address];
+        offerer.tradingAccount = _tradingAccount;
+        offerer.fiatCurrency = _fiatCurrency;
+        offerer.ethQuantity = _ethQuantity;
+        offerer.fiatQuantity = _fiatQuantity;
+        offerer.arbitratorFee = _arbitratorFee;
+
+        offerList.push(offerer);
+    }
+    
+    function getAllOffers() view public returns (Offer[]) {
+        return offerList;
+    }
+    
+     function getOfferCount() view public returns (uint) {
+        return offerList.length;
+    }
+    
+    function getOfferFromAddress(address _address) view public returns(string,string,uint,uint,uint) {
+        return (offers[_address].tradingAccount,offers[_address].fiatCurrency,offers[_address].ethQuantity,offers[_address].fiatQuantity,offers[_address].arbitratorFee);
+    }
+    
+    
+    
+    
+    
+    
+}
