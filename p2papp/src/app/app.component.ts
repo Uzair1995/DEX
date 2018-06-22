e33569148964674c84f5c31e2a00756ec0c5bcd9
Uzair Aleem <uzair.aleem@mparsec.com>
2018-06-22 16:54:22 +0500

    Siple UI for chatting application

--- p2papp/src/app/app.component.ts
+++ p2papp/src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  dataToSend: string;
+
+  SendFunction(){
+    console.log(this.dataToSend);
+    this.dataToSend = "";
+  }
 }
